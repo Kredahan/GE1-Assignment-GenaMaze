20484abe20be641b5b62447dec099573ad104cbd
Kevin Redahan <kevinredahan@gmail.com>
2019-12-18 23:53:03 +0000

    Added Maze Creator Script

---
+++ Assets/Scripts/MazeCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCreator : MonoBehaviour
+{
+    public int mazeRows, mazeColumns; //Maze Rows and Columns are represented by numeric values
+    public GameObject[] wall; // A Unity Asset (Which must be created manually) is used to actually create grid which will become the maze (After walls are destroyed to create a path)
+    public float size = 2f;
+    public Camera Bird;
+
+
+    private MazeCell[,] mazeCells;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
