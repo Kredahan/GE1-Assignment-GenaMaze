ffd904f34c48a7575ccda713b76c8400e30b4db7
Kevin Redahan <kevinredahan@gmail.com>
2019-12-18 23:55:24 +0000

    Added InitializeMaze/Grid Function

--- Assets/Scripts/MazeCreator.cs
+++ Assets/Scripts/MazeCreator.cs
@@ -21,3 +21,44 @@ public class MazeCreator : MonoBehaviour
     void Update()
     {
     }
+
+    private void InitializeMaze()
+    {
+
+        mazeCells = new MazeCell[mazeRows, mazeColumns];
+
+        for (int r = 0; r < mazeRows; r++)
+        {
+            for (int c = 0; c < mazeColumns; c++)
+            {
+                mazeCells[r, c] = new MazeCell();
+                //wall[Random.Range(0, wall.Length)]
+                // For now, use the same wall object for the floor!
+                // mazeCells[r, c].floor = Instantiate(wall, new Vector3(r * size, -(size / 2f), c * size), Quaternion.identity) as GameObject;
+                mazeCells[r, c].floor = Instantiate(wall[0], new Vector3(r * size, -(size / 2f), c * size), Quaternion.identity) as GameObject;
+                mazeCells[r, c].floor.name = "Floor " + r + "," + c;
+                mazeCells[r, c].floor.transform.Rotate(Vector3.right, 90f);
+
+                if (c == 0)
+                {
+                    mazeCells[r, c].westWall = Instantiate(wall[Random.Range(1, wall.Length)], new Vector3(r * size, 0, (c * size) - (size / 2f)), Quaternion.identity) as GameObject;
+                    mazeCells[r, c].westWall.name = "West Wall " + r + "," + c;
+                }
+
+                mazeCells[r, c].eastWall = Instantiate(wall[Random.Range(1, wall.Length)], new Vector3(r * size, 0, (c * size) + (size / 2f)), Quaternion.identity) as GameObject;
+                mazeCells[r, c].eastWall.name = "East Wall " + r + "," + c;
+
+                if (r == 0)
+                {
+                    mazeCells[r, c].northWall = Instantiate(wall[Random.Range(1, wall.Length)], new Vector3((r * size) - (size / 2f), 0, c * size), Quaternion.identity) as GameObject;
+                    mazeCells[r, c].northWall.name = "North Wall " + r + "," + c;
+                    mazeCells[r, c].northWall.transform.Rotate(Vector3.up * 90f);
+                }
+
+                mazeCells[r, c].southWall = Instantiate(wall[Random.Range(1, wall.Length)], new Vector3((r * size) + (size / 2f), 0, c * size), Quaternion.identity) as GameObject;
+                mazeCells[r, c].southWall.name = "South Wall " + r + "," + c;
+                mazeCells[r, c].southWall.transform.Rotate(Vector3.up * 90f);
+            }
+        }
+    }
+}
